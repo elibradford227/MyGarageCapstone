f47a0c29f56da3079235481d2d9ac895992a278a
Eli Bradford <elibradford227@gmail.com>
2023-08-21 20:02:23 -0500

    job delete working

--- pages/jobs/[firebaseKey].js
+++ pages/jobs/[firebaseKey].js
@@ -1,7 +1,52 @@
-import React from 'react';
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Button from 'react-bootstrap/Button';
+import { getSingleJob, deleteJob } from '../../api/jobData';
 
-export default function hi() {
+export default function ViewCar() {
+  const [jobDetails, setJobDetails] = useState({});
+  const router = useRouter();
+
+  // TODO: grab firebaseKey from url
+  const { firebaseKey } = router.query;
+
+  const deleteThisJob = () => {
+    if (window.confirm(`Delete ${jobDetails.title} job?`)) {
+      deleteJob(jobDetails.firebaseKey).then(() => { router.push('/jobs'); });
+    }
+  };
+
+  // TODO: make call to API layer to get the data
+  useEffect(() => {
+    getSingleJob(firebaseKey).then(setJobDetails);
+  }, [firebaseKey]);
+
+  console.warn(jobDetails);
+
   return (
-    <div>hi</div>
+    <div className="mt-5 d-flex flex-wrap" id="carViewContainer">
+      <div
+        id="carColumn"
+        className="d-flex flex-column"
+      >
+        <h2>
+          {jobDetails.title}
+        </h2>
+        <p>{jobDetails.description || ''}</p>
+        <Button variant="danger" onClick={deleteThisJob} className="m-2">
+          DELETE
+        </Button>
+      </div>
+      <div id="carsJobs">
+        <h1 id="jobsh1">Jobs</h1>
+        <Button variant="primary" className="addBtn">Add A Part</Button>
+        <div id="jobsDisplay">
+          {/* {carsJobs.map((job) => (
+            <JobCard key={job.firebaseKey} jobObj={job} />
+          ))} */}
+        </div>
+      </div>
+    </div>
   );
 }

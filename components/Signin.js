f45d1b59fb25a8b93591fa332ce2c0abdd165636
Eli Bradford <elibradford227@gmail.com>
2023-09-02 12:12:19 -0500

    changed sign in page and minor app tweaks

--- components/Signin.js
+++ components/Signin.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { signIn } from '../utils/auth';
@@ -9,13 +10,13 @@ function Signin() {
       style={{
         height: '90vh',
         padding: '30px',
-        maxWidth: '400px',
+        maxWidth: '600px',
         margin: '0 auto',
       }}
     >
-      <h1>Hi there!</h1>
-      <p>Click the button below to login!</p>
-      <Button type="button" size="lg" className="copy-btn" onClick={signIn}>
+      <img src="MyGarageNavLogo.png" alt="logo" />
+      <h4>Click the button below to login!</h4>
+      <Button type="button" size="lg" className="copy-btn" id="signOutBtn" onClick={signIn}>
         Sign In
       </Button>
     </div>
